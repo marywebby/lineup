63a792a97cd40ed9a049a05dd06f90dcf28b3705
Mary Webby <118230483+marywebby@users.noreply.github.com>
2024-03-26 17:55:07 +0000

    created ingredients model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_26_174928) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_26_175415) do
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "type_of_product_id", null: false
